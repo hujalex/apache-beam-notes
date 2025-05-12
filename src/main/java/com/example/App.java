8b7eb9daf0f7bbbc79bce25d6baac987d790cd5e
hujalex <ahuwindsor@gmail.com>
2025-05-12 14:52:09 -0400

    aggregation

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.List;
+import java.util.Arrays;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.options.PipelineOptions;
@@ -12,7 +13,9 @@ import org.apache.beam.sdk.transforms.SerializableFunction;
 import org.apache.beam.sdk.transforms.DoFn.ProcessContext;
 import org.apache.beam.sdk.transforms.DoFn.ProcessElement;
 import org.apache.beam.sdk.transforms.Filter;
+import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.KV;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,14 +28,21 @@ public class App {
         PipelineOptions options = PipelineOptionsFactory.fromArgs(args).create();
         Pipeline pipeline = Pipeline.create(options);
 
-        PCollection<Integer> input = 
-            pipeline.apply(Create.of(1,2,3,4,5,6,7,8,9));
-        
-        PCollection<Integer> filteredInts = applyTransform(input);
+        PCollection<KV<Integer,Integer>> input = pipeline.apply(
+            Create.of(KV.of(1,11),
+            KV.of(1,36),
+            KV.of(2, 91),
+            KV.of(3, 33),
+            KV.of(3, 11),
+            KV.of(4, 33))
+        );
+
+        PCollection<Long> output = applyTransform(input);
+
+        output.apply("Log", ParDo.of(new LogOutput<>("Input has elements")));
 
 
 
-        filteredInts.apply(ParDo.of(new LogIntegers()));
 
         pipeline.run();
 
@@ -47,8 +57,8 @@ public class App {
 
     }
 
-    static PCollection<Integer> applyTransform(PCollection<Integer> input) {
-        return input.apply(Filter.by(number -> number % 2 == 0));
+    static PCollection<Long> applyTransform(PCollection<KV<Integer, Integer>> input) {
+        return input.apply(Count.globally());
     }
 
     public static class LogStrings extends DoFn<String, String> {
@@ -64,6 +74,31 @@ public class App {
         public void processElement(ProcessContext c) throws Exception {
             LOG.info("Processing number: {}", c.element());
             c.output(c.element());
+        }
+    }
+
+    public static class LogLongs extends DoFn<Long, Long> {
+        @ProcessElement
+        public void processElement(ProcessContext c) throws Exception {
+            LOG.info("Processing number: {}", c.element());
+            c.output(c.element());
+        }
+    }
+
+    static class LogOutput<T> extends DoFn<T, T> {
+        private String prefix;
+
+        LogOutput() {
+            this.prefix = "Processing element";
+        }
+
+        LogOutput(String prefix) {
+            this.prefix = prefix;
+        }
+
+        @ProcessElement
+        public void processElement(ProcessContext c) throws Exception {
+            LOG.info(prefix + ": {}", c.element());
         }
     }
 }
